9a0254269c209e796e7f4a1c3e5baf94b0bf4b78
Maksym <admin@worldwideservice.eu>
2025-10-31 15:11:07 +0700

    fix: исправлены импорты Table и Dialog компонентов в UserManagement

--- components/admin/UserManagement.tsx
+++ components/admin/UserManagement.tsx
@@ -20,7 +20,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from '@/components/ui/table'
+} from '@/components/ui/shadcn/table'
 import {
   Dialog,
   DialogContent,
@@ -29,7 +29,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@/components/ui/dialog'
+} from '@/components/ui/shadcn/modal'
 import {
   DropdownMenu,
   DropdownMenuContent,
